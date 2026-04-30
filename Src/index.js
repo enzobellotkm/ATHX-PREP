46cadebd57f15c0f0fd0cd261a8c47d157475e1c
enzobellotkm <bellot.enzo26@gmail.com>
2026-04-30 15:17:55 +0200

    index.js

--- Src/index.js
+++ Src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ATHXPrep from "./App";
+ 
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ATHXPrep />
+  </React.StrictMode>
+);
